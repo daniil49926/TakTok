26aae2864c85c4d5ac14878c65752ae535ec771c
kaMata0 <daniil49926@gmail.com>
2023-07-18 14:36:28 +0300

    added flake8 as a project linter

--- aiohttp_server/core/utils/load_env.py
+++ aiohttp_server/core/utils/load_env.py
@@ -1,6 +1,7 @@
 def load_environ(base_dir: str) -> None:
     """
-    The environment variables file must be on the same level as the base directory of the application
+    The environment variables file must be on the
+        same level as the base directory of the application
     :param base_dir: string value of the base directory of the application
     :type base_dir: str
     """
